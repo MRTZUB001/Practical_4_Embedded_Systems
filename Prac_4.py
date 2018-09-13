97588ae19da12371579521894614a9e862585081
mikedupi <43207667+mikedupi@users.noreply.github.com>
2018-09-13 19:35:44 +0200

    Adding in global variables

---
+++ Prac_4.py
@@ -0,0 +1,9 @@
+# global variables
+values = [0]*8
+t = 0.5 # sample time interval
+timer = 0 # total time elapsed
+names = ['Time','Timer','Pot','Temp','Light']
+stopFlag = True
+monitor = ['']*5
+displayList = monitor*5
+n=0
